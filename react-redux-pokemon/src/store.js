94ba4ee30a5be52fcd0cd5964a3a7f23618c534a
Elise Spain <elisespain@gmail.com>
2019-07-27 23:59:13 -0700

    Store was setup

---
+++ react-redux-pokemon/src/store.js
@@ -0,0 +1,13 @@
+//import createStore & applyMiddleWare come from redux, remember you pass thunk as an argument to applyMiddleware
+//thunk gives us access to dispatch 
+
+import { createStore, applyMiddleware} from 'redux';
+import thunk from 'redux-thunk';
+//import { rootReducer} from '.'
+
+
+//setup your store, invoke createStore 
+//first argument for createStore is going to be rootReducer
+// second argument for createStore is an enhancer, typically known as middleware thunk is always the first middleware , applyMiddleware you invoke and pass in thunk
+
+export const store = createStore(rootReducer, applyMiddleware(thunk));
