5e0115353485e71e069ff6d82c427155b3db45cb
Elise Spain <elisespain@gmail.com>
2019-07-28 17:14:22 -0700

    Components setup

--- react-redux-pokemon/src/components/PokemonList.js
+++ react-redux-pokemon/src/components/PokemonList.js
@@ -5,15 +5,24 @@ import { getPokemon } from '../actions';
 // Components
 import Pokemon from './Pokemon';
 
+// useEffect hook lets you perform side effects in function components
+//some side effects are data fetching, setting up subscription, and manually changing the DOM in React components
 
 
 const PokemonList = (props) => {
     console.log(props);
+    useEffect(()=>{
+      props.getPokemon()  
+    }, [])
+
     return (
         <div>
-            
+        {props.pokemons}    
         </div>
     );
 };
 
-export default PokemonList;
+
+//connect takes two arguments mapStateToProps and mapActionsToProps/ or just an object 
+
+export default connect(mapStateToProps, {getPokemon})(PokemonList);
